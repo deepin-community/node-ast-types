7f5cc6c8f5cf4527988c8ccd04d69e15ef3bc715
Tianyu Chen <sweetyfish@deepin.org>
2023-10-18 22:59:12 +0800

    feat: update node-ast-types to 0.16.1

--- def/babel.ts
+++
@@ -1,8 +0,0 @@
-import { Fork } from "../types";
-import babelCoreDef from "./babel-core";
-import flowDef from "./flow";
-
-export default function (fork: Fork) {
-  fork.use(babelCoreDef);
-  fork.use(flowDef);
-};
